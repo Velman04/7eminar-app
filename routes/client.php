5444b934295cd981e3a7f677c934d40c9b4a117c
Vladyslav Hovorushko <vladyslav.hovorushko@drumncode.com>
2024-08-15 13:15:51 +0300

    Remove test route && controller

--- routes/client.php
+++ routes/client.php
@@ -2,7 +2,4 @@
 
 declare(strict_types=1);
 
-use App\Application\Client\Controllers\Status\ClientStatusController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/status', ClientStatusController::class);
